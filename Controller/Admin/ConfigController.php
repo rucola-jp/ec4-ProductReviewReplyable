c5686a4ae41b764e31f4255069fd226e0ced112d
rucola-jp <info@ruco.la>
2019-07-02 07:29:06 +0000

    first commit

---
+++ Controller/Admin/ConfigController.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\ProductReviewReplyable\Controller\Admin;
+
+use Plugin\ProductReviewReplyable\Form\Type\Admin\ProductReviewConfigType;
+use Plugin\ProductReviewReplyable\Repository\ProductReviewConfigRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class ConfigController.
+ */
+class ConfigController extends \Eccube\Controller\AbstractController
+{
+    /**
+     * @Route("/%eccube_admin_route%/product_review/config", name="product_review4_admin_config")
+     * @Template("@ProductReviewReplyable/admin/config.twig")
+     *
+     * @param Request $request
+     * @param ProductReviewConfigRepository $configRepository
+     *
+     * @return array
+     */
+    public function index(Request $request, ProductReviewConfigRepository $configRepository)
+    {
+        $Config = $configRepository->get();
+        $form = $this->createForm(ProductReviewConfigType::class, $Config);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $Config = $form->getData();
+            $this->entityManager->persist($Config);
+            $this->entityManager->flush($Config);
+
+            log_info('Product review config', ['status' => 'Success']);
+            $this->addSuccess('product_review.admin.save.complete', 'admin');
+
+            return $this->redirectToRoute('product_review4_admin_config');
+        }
+
+        return [
+            'form' => $form->createView(),
+        ];
+    }
+}
---
+++ ProductReviewNav.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\ProductReviewReplyable;
+
+use Eccube\Common\EccubeNav;
+
+class ProductReviewNav implements EccubeNav
+{
+    public static function getNav()
+    {
+        return [
+            'product' => [
+                'children' => [
+                    'product_review' => [
+                        'name' => 'product_review.admin.product_review.title',
+                        'url' => 'product_review_admin_product_review',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
